f7f602ebc5464e14eabbc67834a63f83da93b9fe
acloshh <santitornau@gmail.com>
2026-05-06 18:24:17 -0300

    se updateo phaser y el titulo del juego

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -16,10 +16,10 @@ export default class HelloWorldScene extends Phaser.Scene {
 
   preload() {
     // load assets
-HEAD
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
+
+    this.load.image("sky", "./public/assets/space3.png");
+    this.load.image("logo", "./public/assets/phaser3-logo.png");
+    this.load.image("red", "./public/assets/particles/red.png");
 
     this.load.image("sky", "./public/assets/space3.png");
     this.load.image("logo", "./public/assets/phaser3-logo.png");
